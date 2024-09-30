7cfc0d7c60fc3f86f45e46924604943eaf4bc6c4
Paul L. <13600558+dev-paulL@users.noreply.github.com>
2024-09-30 14:24:11 +0200

    added:hover effect & readme/fix:responsive, accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import mobileSeparator from "./assets/pattern-divider-mobile.svg";
+import desktopSeparator from "./assets/pattern-divider-desktop.svg";
 import diceImg from "./assets/icon-dice.svg";
 import { useEffect, useState } from "react";
 
@@ -13,6 +14,7 @@ function App() {
 
   const fetchAdvice = async () => {
     try {
+      setLoading(true);
       const response = await fetch("https://api.adviceslip.com/advice");
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -30,25 +32,35 @@ function App() {
     fetchAdvice();
   }, []);
 
-  if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
   return (
     <main className="bg-darkBlue min-h-screen flex items-center justify-center p-4">
-      <article className="relative bg-darkGrayishBlue rounded-lg p-6 text-center flex flex-col gap-8 items-center max-w-[50rem] w-4/12">
-        <h1 className="text-sm font-bold tracking-[.25em] uppercase text-neonGreen">
+      <article className="shadow-xl relative bg-darkGrayishBlue rounded-lg p-6 text-center flex flex-col gap-8 items-center justify-items-center max-w-[50rem] w-full md:w-[70%] lg:w-[50%]">
+        <h1
+          aria-live="assertive"
+          className="text-sm font-bold tracking-[.25em] uppercase text-neonGreen"
+        >
           Advice #{advice?.id}
         </h1>
 
-        <blockquote className="relative text-quote text-lightCyan after:content-['»'] before:content-['«']">
+        <blockquote
+          aria-live="assertive"
+          className="relative text-quote text-lightCyan after:content-['»'] before:content-['«']"
+        >
           {advice?.advice}
         </blockquote>
 
-        <img src={mobileSeparator} alt="" className="w-full pb-10" />
+        <picture className="w-full pb-10">
+          <source srcSet={desktopSeparator} media="(min-width: 64rem)" />
+          <img src={mobileSeparator} alt="" className="block mx-auto" />
+        </picture>
 
         <button
-          className="absolute cursor-pointer bottom-0 translate-y-8 rounded-full p-5 bg-neonGreen"
+          aria-label="Generate new advice"
+          className="absolute hover:shadow-neon duration-300 transition-shadow cursor-pointer bottom-0 translate-y-8 rounded-full p-5 bg-neonGreen"
           onClick={fetchAdvice}
+          disabled={loading}
         >
           <img src={diceImg} alt="" />
         </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         lightCyan: "hsl(193, 38%, 86%)",
         neonGreen: "hsl(150, 100%, 66%)",
         grayishBlue: "hsl(217, 19%, 38%)",
-        darkGrayishBlue: " hsl(217, 19%, 24%)",
+        darkGrayishBlue: "hsl(217, 19%, 24%)",
         darkBlue: "hsl(218, 23%, 16%)",
       },
       fontFamily: {
@@ -15,6 +15,9 @@ export default {
       },
       fontSize: {
         quote: "1.75rem",
+      },
+      boxShadow: {
+        neon: "0 0 1.3rem 0.8rem rgba(82, 255, 168, 0.5)",
       },
     },
   },
